03359099b626cc325e75b9558d07fbff4df2885e
Eyal Ben Ivri <eyalbenivri@google.com>
2022-08-18 14:17:36 +0200

    Added shell script to setup tests - to be used in tutorial
    Change-Id: I2a91b3100a35cfcf343f9d27c386d737133dd101

---
+++ setup_tests.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env sh
+
+# [START setup_status_test]
+cat <<EOF > sql_tests/status_is_valid.sql
+SELECT
+IF
+  (
+    -- The test
+    status IN ('Shipped',
+      'Complete',
+      'Returned',
+      'Cancelled',
+      'Processing'),
+
+    -- The pass message (per record)
+    CONCAT('status ', status, ' is valid'),
+
+    -- The error message
+    ERROR(CONCAT('Status ', status, ' is not valid at record with unique_key=', order_id)) ) AS status_is_correct
+FROM
+  `${thelook_ecom_copy.orders}`;
+EOF
+# [END setup_status_test]
+
+# [START setup_no_missing_users_test]
+cat <<EOF > sql_tests/no_missing_users_in_orders_table.sql
+ASSERT
+  ((
+    SELECT
+      COUNT(*)
+    FROM (
+      SELECT
+        *
+      FROM
+        `${thelook_ecommerce.orders}` AS o
+      LEFT JOIN
+        `${thelook_ecommerce.users}` AS u
+      ON
+        u.id = o.user_id
+      WHERE
+        u.id IS NULL)) = 0) AS "No invalid user_id in orders table";
+EOF
+# [END setup_no_missing_users_test]
+
+# [START setup_no_missing_orders_test]
+cat <<EOF > sql_tests/no_missing_orders_in_order_items_table.sql
+ASSERT
+  ((
+    SELECT
+      COUNT(*)
+    FROM (
+      SELECT
+        *
+      FROM
+        `${thelook_ecommerce.order_items}` AS oi
+      LEFT JOIN
+        `${thelook_ecommerce.orders}` AS o
+      ON
+        o.order_id = oi.order_id
+      WHERE
+        o.order_id IS NULL)) = 0) AS "No invalid order_id in orders table";
+EOF
+# [END setup_no_missing_orders_test]
