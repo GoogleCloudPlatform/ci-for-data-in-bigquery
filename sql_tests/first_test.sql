edc0589eb8b7bdd9bb14463d86d7100e41dc0c36
Eyal Ben Ivri <eyalbenivri@google.com>
2022-08-19 15:31:48 +0200

    added pseudo test
    Change-Id: Ib211cc26cf8d53b087f56654cd67553f237a6287

---
+++ sql_tests/first_test.sql
@@ -0,0 +1,4 @@
+-- This is a pseudo test place holder. It does nothing to actually test data,
+-- but rather just a dry run to verify BQ client can connect.
+-- Feel free to remove this file, it is not required
+SELECT 1;
