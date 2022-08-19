837191dcab8089ace85635bc36c3da7d61b1f97a
Eyal Ben Ivri <eyalbenivri@google.com>
2022-08-19 18:54:54 +0200

    delete test files (we have a bash generator)
    Change-Id: Ifc0b5e59d5cdaf01da720758cdcac8b8d18fd983

--- sql_tests/no_missing_users_in_orders_table.sql
+++
@@ -1,8 +0,0 @@
-ASSERT(
-    (SELECT COUNT(*) FROM (
-        SELECT o.user_id
-        FROM `${thelook_ecommerce.orders}` AS o
-        LEFT JOIN `${thelook_ecommerce.users}` AS u ON u.id = o.user_id
-        WHERE o.user_id IS NULL)
-    ) = 0
-) AS "No invalid user_id in orders table";
